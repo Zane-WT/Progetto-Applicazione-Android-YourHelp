01ffcd2fd4b2979bfad51ef983321dc23fd71aa7
Luca Zanetti <zanetti.luca.22@itisriva.edu.it>
2026-04-10 20:36:27 +0200

    Prova passaggio di finestra (testato)

--- app/src/main/java/com/example/yourhelp/SecondActivity.java
+++ app/src/main/java/com/example/yourhelp/SecondActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,28 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            Button button = findViewById(R.id.button1);
-            ;
             return insets;
         });
-        Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button indietro = findViewById(R.id.backButton);
+        indietro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intento = new Intent(MainActivity.this,SecondActivity.class);
-                intento.putExtra("Chiave_Messaggio","Ciao dalla prima finestra");
-                startActivity(intento);
+                finish();
             }
         });
+
     }
 }
