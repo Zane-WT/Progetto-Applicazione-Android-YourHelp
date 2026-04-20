b0d627f0aeb803a62c0dcee6ad64ce39c3af36fa
Taisia <atyasova.taisia.22@itisriva.edu.it>
2026-04-20 19:28:58 +0200

    aggiunta taisia nomi pagine stabilite iniziali

--- app/src/main/java/com/example/yourhelp/MainActivity.java
+++ app/src/main/java/com/example/yourhelp/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.yourhelp;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
